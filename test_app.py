a691623e27c5deff0db642b33ed52140f5baea5f
Florian Portmann <89086527+florian-portmann@users.noreply.github.com>
2023-10-27 14:51:44 +0200

    Add files via upload

--- test_app.py
+++ test_app.py
@@ -2,27 +2,38 @@ from app import app, entries
 
 import pytest
 
-# Use Flask's test client for testing
-
 
 @pytest.fixture()
 def client():
-    app.config['TESTING'] = True
+    app.config["TESTING"] = True
     client = app.test_client()
 
     yield client
 
 
 def test_add_entry(client):
-    # Test adding an entry
+    response = client.post("/add_entry", data={"content": "Test Entry Content"})
+
+    assert response.status_code == 302
+    assert response.headers["Location"] == "/"
+
+    entry = entries[0]
+    assert entry is not None
+    assert entry.content == "Test Entry Content"
+
+
+def test_add_entry_with_happiness(client):
+    # Test adding an entry with happiness
     response = client.post(
-        '/add_entry', data={'content': 'Test Entry Content'})
+        "/add_entry", data={"content": "Test Entry Content", "happiness": "😃"}
+    )
 
     # Check if the response is a redirect to the index page
     assert response.status_code == 302
-    assert response.headers['Location'] == '/'
+    assert response.headers["Location"] == "/"
 
-    # Check if the entry was added to the database
+    # Check if the entry was added to the database with the correct happiness
     entry = entries[0]
     assert entry is not None
-    assert entry.content == 'Test Entry Content'
+    assert entry.content == "Test Entry Content"
+    assert entry.happiness == "😃"
